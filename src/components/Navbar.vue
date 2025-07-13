ac986550370a77d7bd53b1f04e2d03eab5b1a12f
setyoaldi <achmadsetyoaldi@gmail.com>
2025-07-13 12:03:03 +0700

    style : add Navbar and installing some dependencies

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="border-b-[2px] h-20 border-neutral-200">
+    <div class="px-7 py-5 flex justify-between">
+      <img src="../assets/jeje-logo.png" class="object-cover" width="100" />
+      <div class="flex gap-4">
+        <NuxtLink
+          to="/"
+          class="text-neutral-500"
+          active-class="font-bold text-orange-500 "
+        >
+          Shipment
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
